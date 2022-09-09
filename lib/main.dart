3ccba8869e8134440bcf4570bc333d791309cdbd
Simon Hedlund <shedlundh@gmail.com>
2022-09-09 10:55:25 +0200

    dropdown menu i appbar,lmargin på knapp, add 2View

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
- @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MainView(),
@@ -19,97 +18,123 @@ class MainView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TIG 169 TODO", 
-        style: TextStyle(
-          color: Colors.black 
-            )
-          ),
+        title: Text("TIG 333 TODO", style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.grey[400],
-      ) ,
-      body: Center(
-        child: Column(children: [
-          _todoRow(),
-          Divider(color: Colors.black,),
-          _todoRow(),
-          Divider(color: Colors.black,),
-          //plusButton(),
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(context,
-                MaterialPageRoute(builder: (context) => SecondView()));}),
-          
-        ]
-        )
+        actions: [
+          _popMenu()
+  ] 
       ),
+      body: Center(
+          child: Column(children: [
+        Container(height: 10,),
+        _todoRow(),
+        Divider(
+          color: Colors.black,
+        ),
+        _todoRow(),
+        Divider(
+          color: Colors.black,
+        ),
+    
 
+        Expanded(
+            child: Align(
+          alignment: FractionalOffset.bottomRight,
+          child: Container(
+            margin: EdgeInsets.only(right: 30, bottom: 30),
+            child: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SecondView()));
+              })),
+        ))
+      ])),
     );
   }
 
-  Widget _todoRow(){
+  Widget _popMenu() {
+    return PopupMenuButton(
+          icon: Icon(Icons.more_vert),
+          itemBuilder: (context){
+            return [
+                  PopupMenuItem(
+                      child: Text("All"),
+                  ),
+
+                  PopupMenuItem(
+                      child: Text("Done"),
+                  ),
+
+                  PopupMenuItem(
+                      child: Text("Undone"),
+                  ),
+              ];
+          }
+        );
+  }
+
+  Widget _todoRow() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Checkbox(
-          value: false, 
+          value: false,
           onChanged: (val) {},
-          ),
+        ),
         Text("Mjölk"),
-        ElevatedButton(onPressed: (){}, child: Icon(Icons.clear))
+        Container(
+          margin:EdgeInsets.only(right: 10) ,
+          child: ElevatedButton(onPressed: () {}, child: Icon(Icons.clear)))
       ],
     );
   }
 
-  Widget plusButton() {
+  Widget plusButton() { //Dennaa används ej, pga navigator funkar ej här
     return Positioned(
-      bottom: 30,
-      right: 30,
-      child: 
-      FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-         /* Navigator.push(context,
+        bottom: 30,
+        right: 30,
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            /* Navigator.push(context,
           MaterialPageRoute(builder: (context) => SecondView()));
-        */},
-        
-       )
-    );
+        */
+          },
+        ));
   }
-
 }
 
+//SECOND VIEW
 class SecondView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TIG 169 TODO", 
-        style: TextStyle(
-          color: Colors.black 
-          )
-          ),
+        title: Text("TIG 333 TODO", style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.grey[400],
-      ) ,
-      body: Center(
-        child: Column(children: [
-          Container( //FKYTTAS TILL SECOND VIEW, MEN VARFÖR FUNKAR EJ MARGINS? 
-            margin: EdgeInsets.only(left: 40.0, right: 40) ,
-            child: TextField(
-              decoration: InputDecoration( 
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 2)
-                ), 
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 2)
-                ),
-                hintText: "Vad mer behöver du göra?" 
-              ),
-            )
-          ) //SKA FLYTTAS HELA VÄGEN HIT 
-        ]
-        )
       ),
-
-    ); 
+      body: Center(
+          child: Column(children: [
+            Container(height: 30,),
+            todoInput(),
+            Container(height: 30,),
+            ElevatedButton(onPressed: () {}, child: Text("+ ADD"))
+      ])),
+    );
   }
-}
 
+  Widget todoInput() {
+    return Container(
+            margin: EdgeInsets.only(left: 40.0, right: 40),
+            child: TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2)),
+                  hintText: "Vad mer behöver du göra?"),
+            ));
+  }
+
+
+}
